68439b0548dcf23fef287a370f20108c8ac97ede
Nikita <ndyachenko2196@gmail.com>
2021-06-29 00:12:02 +0300

    homework 5 commit #2

---
+++ src/test/java/ru/training/at/hw5/driverutils/DriverManager.java
@@ -0,0 +1,24 @@
+package ru.training.at.hw5.driverutils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverManager implements DriverInterface {
+
+    private WebDriver driver;
+
+    public WebDriver setupDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts()
+                    .implicitlyWait(10, TimeUnit.SECONDS);
+        }
+        return driver;
+    }
+
+}
